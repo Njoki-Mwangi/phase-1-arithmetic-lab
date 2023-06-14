b9ccd2e041e10f51a248f25730067487f473d02b
Njoki-Mwangi <faithgracem96@gmail.com>
2023-06-15 01:59:35 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+// let multiply = num1 * num2;
+// //number num1;
+// //number num2;
+// //(num1 x num2){
+//   //  return multiply = 62;
+// //}
 
-// Write your code here
+
+// let random = Math.floor(Math.random() * 10) + 1;
+// //Math.random > 0;
+
+// let mod = num3 % num4;
+// //number num3;
+// //number num2;
+// //mod = num3 % num2;{
+//     //return mod = 4;
+
+// let max = Math.max(1, 5, 10, 15, 20);
+//    // return Math.max = 20;
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+let set = [1, 5, 10, 15, 20];
+let max = Math.max(...set);
